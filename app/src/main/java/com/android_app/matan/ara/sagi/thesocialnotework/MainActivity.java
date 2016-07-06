2ca5d59ee2a25707050476b9492c3a088632a3e2
Aran Zaiger <aranzaiger@gmail.com>
2016-07-06 20:30:57 +0300

    Merge branch 'master' of https://github.com/sagidayan/TheSocialNotework-Android
    # Conflicts:
    #	.idea/libraries/animated_vector_drawable_23_3_0.xml
    #	.idea/libraries/appcompat_v7_23_3_0.xml
    #	.idea/libraries/design_23_3_0.xml
    #	.idea/libraries/recyclerview_v7_23_3_0.xml
    #	.idea/libraries/support_annotations_23_4_0.xml
    #	.idea/libraries/support_v4_23_3_0.xml
    #	.idea/libraries/support_vector_drawable_23_3_0.xml
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/android_app/matan/ara/sagi/thesocialnotework/MainActivity.java
+++ app/src/main/java/com/android_app/matan/ara/sagi/thesocialnotework/MainActivity.java
@@ -17,14 +17,29 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    protected final String TAG = "[TSN / MainActivity]";
+    protected static final String TAG = "[TSN / MainActivity]";
     protected String userId;
     private GPSUtils gpsUtils;
     private boolean locationPermission;
     public static ProgressDialog progress;
+    private GmapFragment gmapFragment;
+    private PersonalFragment personalFragment;
+    public static final String BASE_URL = "http://thesocialnotework-api.appspot.com/api";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,8 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        gmapFragment = new GmapFragment();
+        personalFragment = new PersonalFragment();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -58,7 +75,7 @@ public class MainActivity extends AppCompatActivity
 
         //Change Layout
         Log.d(TAG, "Changing Fragment to Personal Activity");
-        PersonalFragment personalFragment = new PersonalFragment();
+//        PersonalFragment personalFragment = new PersonalFragment();
         personalFragment.setArguments(b);
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, personalFragment);
@@ -104,15 +121,26 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Bundle b = new Bundle();
+
         int id = item.getItemId();
 
         if (id == R.id.nav_explore) {
             // Handle the camera action
         } else if (id == R.id.nav_map) {
-
+//
+            Log.d(TAG,"Before going to map");
+//            gmapFragment.("note_list", (ArrayList<Note>) listOfNotes);
+//            gmapFragment.put("user_lat", gpsUtils.getLatitude());
+//            gmapFragment.putExtra("user_lng", gpsUtils.getLongitude());
+            ft.replace(R.id.fragment_container, gmapFragment);
+            ft.commit();
         } else if (id == R.id.nav_personal) {
 
+            Log.d(TAG,"Before going to personal");
+            ft.replace(R.id.fragment_container, personalFragment);
+            ft.commit();
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_logout) {
@@ -122,7 +150,9 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }public GPSUtils getGPSUtils() {
+    }
+
+    public GPSUtils getGPSUtils() {
         return this.gpsUtils;
     }
 
@@ -149,6 +179,48 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    //    //Generic response ErrorListener
+    Response.ErrorListener genericErrorListener = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            Log.d(TAG, "genericErrorListener");
+            error.printStackTrace();
+        }
+    };
 
+        public Note getNoteFromJsonObj(JSONObject noteObject, Date time) throws JSONException {
+//            List<Note> listOfNotes = new ArrayList<>();
+
+            Note note = new Note(
+                noteObject.getString("id"),
+                Float.parseFloat(noteObject.getJSONObject("location").getString("lat")),
+                Float.parseFloat(noteObject.getJSONObject("location").getString("lng")),
+                noteObject.getJSONObject("location").getString("address"),
+                noteObject.getString("title"),
+                noteObject.getString("body"),
+                time.toString(),
+                noteObject.getBoolean("is_public"),
+                noteObject.getInt("likes"),
+                jsonArrayToStringArray(noteObject.getJSONArray("tags"))
+        );
+            return note;
+//        listOfNotes.add(addNote);
+
+    }
+
+    public ArrayList<String> jsonArrayToStringArray(JSONArray jArray) {
+        ArrayList<String> stringArray = new ArrayList<String>();
+        for (int i = 0, count = jArray.length(); i < count; i++) {
+            try {
+                JSONObject jsonObject = jArray.getJSONObject(i);
+                stringArray.add(jsonObject.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return stringArray;
+    }
+
+    public String getUserId(){return userId;}
 
 }
