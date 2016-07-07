b263d3d9c1610f5ab4f7ea26369545ed076da87c
Aran Zaiger <aranzaiger@gmail.com>
2016-07-07 23:42:12 +0300

    added map Features
    - added zoom limitation
    - added avater loading in background
    - remove self marker
    - added LruBitmapCache
    - added getBitmapFromURL

--- app/src/main/java/com/android_app/matan/ara/sagi/thesocialnotework/GmapFragment.java
+++ app/src/main/java/com/android_app/matan/ara/sagi/thesocialnotework/GmapFragment.java
@@ -157,16 +157,6 @@ public class GmapFragment extends Fragment implements OnMapReadyCallback {
         }
     };
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
 
 
     public Bitmap getBitmapFromURL(String imageUrl) {
